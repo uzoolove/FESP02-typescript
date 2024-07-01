09f6f31fb9d1a1a9098bdde61704299fb2ec4af5
Kilyong Jeong <uzoolove@gmail.com>
2024-07-01 17:37:35 +0900

    ✨

--- workspace-ins/ch01/17.ts
+++ workspace-ins/ch01/17.ts
@@ -2,5 +2,23 @@
 // 16.ts 복사
 
 (()=>{
-  
+  interface Todo {
+    title: string;
+    content: string;
+  }
+  interface Todo {
+    id: number,
+    title: string,
+    // title: number,
+    done: boolean,
+  }
+
+  const todo3: Todo = {
+    title: "할일 3",
+    content: "상세 조회에 사용",
+    id: 123,
+    done: false
+  }
+
+  console.log(todo3);
 })();
--- workspace-ins/dist/ch01/17.js
+++ workspace-ins/dist/ch01/17.js
@@ -2,4 +2,11 @@
 // 인터페이스 선언 병합
 // 16.ts 복사
 (() => {
+    const todo3 = {
+        title: "할일 3",
+        content: "상세 조회에 사용",
+        id: 123,
+        done: false
+    };
+    console.log(todo3);
 })();
