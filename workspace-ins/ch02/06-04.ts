2ee413de9b7c1b3da0f4c9625e266a5f12839699
Kilyong Jeong <uzoolove@gmail.com>
2024-07-02 17:35:25 +0900

    ✨

--- workspace-ins/ch02/06-04.ts
+++ workspace-ins/ch02/06-04.ts
@@ -2,5 +2,34 @@
 // 06-03.ts 복사
 
 (()=>{
-  
+  interface DropdownItem<T extends string | number> {
+    value: T;
+    selected?: boolean;
+  }
+
+  const cityList: DropdownItem<string>[] = [
+    { value: 'Seoul', selected: false },
+    { value: 'busan' },
+    { value: 'GwangJu', selected: true },
+    // { value: 56789 },
+  ];
+
+  const zipcodeList: DropdownItem<number>[] = [
+    { value: 12345, selected: false },
+    { value: 34567, selected: true },
+    { value: 56789 },
+  ];
+
+  // function createDropdownList(list: DropdownItem<string | number>[]){
+  function createDropdownList<T extends string | number>(list: DropdownItem<T>[]){
+    let value: T;
+    const options = list.map(item => {
+      value = item.value;
+      return `<option selected="${ item.selected ? 'selected' : '' }">${ typeof value === 'string' ? value.toLowerCase() : value }</option>`;
+    });
+    return `<select>\n${ options.join('\n') }\n</select>`;   
+  }
+
+  console.log(createDropdownList<string>(cityList));
+  console.log(createDropdownList<number>(zipcodeList));
 })();
