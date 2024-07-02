f9b1c469f2e649867aea023abd0df943f6b0d2e6
Kilyong Jeong <uzoolove@gmail.com>
2024-07-02 10:26:20 +0900

    ✨

--- workspace-ins/ch01/18-02.ts
+++ workspace-ins/ch01/18-02.ts
@@ -2,5 +2,16 @@
 // 18-01.ts 복사
 
 (()=>{
-  
+  let ts = 'TypeScript';
+  let nextjs = 'Next.js';
+  function getSchedule(subject: string) {
+    if(ts = subject){
+      return `타입스크립트 수업은 이론 1주 + 프로젝트 1주 입니다.`;
+    }else if(nextjs === subject){
+      return `넥스트 JS 수업은 이론 2주 + 프로젝트 4주 입니다.`;
+    }
+  }
+
+  console.log(getSchedule(ts));
+  console.log(getSchedule(nextjs));
 })();
--- workspace-ins/ch01/18-03.ts
+++ workspace-ins/ch01/18-03.ts
@@ -2,5 +2,16 @@
 // 18-02.ts 복사
 
 (()=>{
-  
+  const TYPE_SCRIPT = 'TypeScript';
+  const NEXT_JS = 'Next.js';
+  function getSchedule(subject: string) {
+    if(TYPE_SCRIPT === subject){
+      return `타입스크립트 수업은 이론 1주 + 프로젝트 1주 입니다.`;
+    }else if(NEXT_JS === subject){
+      return `넥스트 JS 수업은 이론 2주 + 프로젝트 4주 입니다.`;
+    }
+  }
+
+  console.log(getSchedule(TYPE_SCRIPT));
+  console.log(getSchedule(NEXT_JS));
 })();
