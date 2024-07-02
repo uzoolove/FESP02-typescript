1a19d503de0de8cfb925db1cd91618df3b5806e2
Kilyong Jeong <uzoolove@gmail.com>
2024-07-02 17:14:58 +0900

    ✨

--- workspace-ins/ch02/06-02.ts
+++ workspace-ins/ch02/06-02.ts
@@ -2,5 +2,39 @@
 // 06-01.js 복사
 
 (()=>{
-  
+
+  interface ICity {
+    value: string;
+    selected?: boolean;
+  }
+
+  interface IZipcode {
+    value: number;
+    selected?: boolean;
+  }
+
+  const cityList: ICity[] = [
+    { value: 'Seoul', selected: false },
+    { value: 'busan' },
+    { value: 'GwangJu', selected: true },
+    // { value: 56789 },
+  ];
+
+  const zipcodeList: IZipcode[] = [
+    { value: 12345, selected: false },
+    { value: 34567, selected: true },
+    { value: 56789 },
+  ];
+
+  function createDropdownList(list: (ICity | IZipcode)[]){
+    let value;
+    const options = list.map(item => {
+      value = item.value;
+      return `<option selected="${ item.selected ? 'selected' : '' }">${ typeof value === 'string' ? value.toLowerCase() : value }</option>`;
+    });
+    return `<select>\n${ options.join('\n') }\n</select>`;   
+  }
+
+  console.log(createDropdownList(cityList));
+  console.log(createDropdownList(zipcodeList));
 })();
--- workspace-ins/ch02/06-03.ts
+++ workspace-ins/ch02/06-03.ts
@@ -2,5 +2,33 @@
 // 06-02.ts 복사
 
 (()=>{
-  
+  interface DropdownItem {
+    value: string | number;
+    selected?: boolean;
+  }
+
+  const cityList: DropdownItem[] = [
+    { value: 'Seoul', selected: false },
+    { value: 'busan' },
+    { value: 'GwangJu', selected: true },
+    // { value: 56789 },
+  ];
+
+  const zipcodeList: DropdownItem[] = [
+    { value: 12345, selected: false },
+    { value: 34567, selected: true },
+    { value: 56789 },
+  ];
+
+  function createDropdownList(list: DropdownItem[]){
+    let value;
+    const options = list.map(item => {
+      value = item.value;
+      return `<option selected="${ item.selected ? 'selected' : '' }">${ typeof value === 'string' ? value.toLowerCase() : value }</option>`;
+    });
+    return `<select>\n${ options.join('\n') }\n</select>`;   
+  }
+
+  console.log(createDropdownList(cityList));
+  console.log(createDropdownList(zipcodeList));
 })();
