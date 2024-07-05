79537042a975b7b5eb26cf274887d8b16f344c1b
Kilyong Jeong <uzoolove@gmail.com>
2024-07-05 12:21:25 +0900

    ✨

--- workspace-ins/todo-js/src/App.jsx
+++ workspace-ins/todo-js/src/App.jsx
@@ -1,35 +1,44 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="container mx-auto p-4">
+      <h1 className="text-4xl font-bold text-center mb-8">Todo App</h1>
+      <div className="bg-white rounded-lg shadow-lg p-6">
+        <div className="flex mb-4">
+          <input className="flex-grow border-2 border-gray-300 p-2 rounded-lg focus:outline-none focus:border-orange-500" type="text" placeholder="할일을 입력하세요." />
+          <button className="bg-blue-500 hover:bg-blue-600 py-2 px-4 ml-2 text-base text-white rounded" type="button">추가</button>
+        </div>
+        <ul>
+          <li className="flex justify-between p-2 border-b-2 border-gray-200">
+            <form className="flex-grow flex item-center">
+              <span className="cursor-pointer">리액트 다시보기</span>
+              <div className="flex ml-auto">
+                <button className="bg-gray-500 hover:bg-gray-600 py-2 px-4 ml-2 text-sm text-white rounded" type="button">수정</button>
+                <button className="bg-red-500 hover:bg-red-600 py-2 px-4 ml-2 text-sm text-white rounded" type="button">삭제</button>
+              </div>
+            </form>
+          </li>
+          <li className="flex justify-between p-2 border-b-2 border-gray-200">
+            <form className="flex-grow flex item-center">
+              <span className="cursor-pointer">자바스크립트 복습</span>
+              <div className="flex ml-auto">
+                <button className="bg-gray-500 hover:bg-gray-600 py-2 px-4 ml-2 text-sm text-white rounded" type="button">수정</button>
+                <button className="bg-red-500 hover:bg-red-600 py-2 px-4 ml-2 text-sm text-white rounded" type="button">삭제</button>
+              </div>
+            </form>
+          </li>
+          <li className="flex justify-between p-2 border-b-2 border-gray-200">
+            <form className="flex-grow flex item-center">
+              <input className="flex-grow border-2 border-gray-300 p-1" type="text" value="타입스크립트 공부" placeholder="내용을 입력하세요." />
+              <div className="flex ml-auto">
+                <button className="bg-blue-500 hover:bg-blue-600 py-2 px-4 ml-2 text-sm text-white rounded" type="button">저장</button>
+                <button className="bg-gray-500 hover:bg-gray-600 py-2 px-4 ml-2 text-sm text-white rounded" type="button">취소</button>
+              </div>
+            </form>
+          </li>
+        </ul>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </div>
+  );
 }
 
 export default App
--- workspace-ins/todo-js/vite.config.js
+++ workspace-ins/todo-js/vite.config.js
@@ -4,4 +4,12 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: [
+      { find: "@", replacement: "/src" },
+      { find: "@components", replacement: "/src/components" },
+      { find: "@pages", replacement: "/src/pages" },
+      { find: "@hooks", replacement: "/src/hooks" },
+    ],
+  },
 })
